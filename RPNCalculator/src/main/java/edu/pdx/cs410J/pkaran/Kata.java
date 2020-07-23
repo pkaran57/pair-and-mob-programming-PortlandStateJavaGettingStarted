c66f4c9b85290d3fc9f28b62cb59b7454a02d241
Karan <karanpatel4567@gmail.com>
2020-07-22 18:35:25 -0700

    Adding evaluateRPNStack

--- RPNCalculator/src/main/java/edu/pdx/cs410J/pkaran/Kata.java
+++ RPNCalculator/src/main/java/edu/pdx/cs410J/pkaran/Kata.java
@@ -1,16 +1,58 @@
 package edu.pdx.cs410J.pkaran;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 /**
- * A class for getting started with a code kata
- *
- * Use IntelliJ's "Refactor | Rename..." command to change the name of this
- * class (and its tests).
+ * RPN Calculator
+ * <p>
+ * An RPN calculator computes expressions written in Reverse Polish Notation.
+ * http://codingdojo.org/kata/RPN/
  */
 public class Kata {
-                                                                                    
 
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
-  }
+
+    public static void main(String[] args) throws Exception {
+        String expression = "3 5 8 * 7 + *";
+        List<String> expressionTokens = Arrays.asList(expression.split(" "));
+
+        Stack<String> stack = new Stack<>();
+
+        for (String token : expressionTokens) {
+            stack.push(token);
+        }
+
+        System.out.println(evaluateRPNStack(stack));
+    }
+
+    private static double evaluateRPNStack(Stack<String> rpnStack) throws Exception {
+        String token = rpnStack.pop();
+        double a, b;
+
+        try {
+            a = Double.parseDouble(token);
+        } catch (Exception e) {
+            b = evaluateRPNStack(rpnStack);
+            a = evaluateRPNStack(rpnStack);
+
+            switch (token) {
+                case "+":
+                    a += b;
+                    break;
+                case "-":
+                    a -= b;
+                    break;
+                case "*":
+                    a *= b;
+                    break;
+                case "/":
+                    a /= b;
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+        return a;
+    }
 }
